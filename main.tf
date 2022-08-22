a9c084b60f4b88010f91e9af8702260704f0c2dc
Cody Kuykendall <cwk29@icloud.com>
2022-08-22 14:06:55 -0400

    Remove aws profile

--- main.tf
+++ main.tf
@@ -1,9 +1,5 @@
 terraform {
   required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 2.13.0"
-    }
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.16"
@@ -25,24 +21,7 @@ terraform {
   }
 }
 
-provider "docker" {}
-
-resource "docker_image" "nginx" {
-  name         = "nginx:latest"
-  keep_locally = false
-}
-
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.latest
-  name  = "tutorial"
-  ports {
-    internal = 80
-    external = 8000
-  }
-}
-
 provider "aws" {
-  profile = "wortech"
   region  = "us-east-1"
 }
 
