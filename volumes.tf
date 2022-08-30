c838c6dabad1940abfdcda5a9691fac9b92e3317
Cody Kuykendall <cwk29@icloud.com>
2022-08-29 22:38:17 -0400

    Run fmt for aws_ebs_volume

--- volumes.tf
+++ volumes.tf
@@ -1,7 +1,7 @@
 resource "aws_ebs_volume" "mongo" {
   availability_zone = data.aws_availability_zones.available.names[0]
   size              = 1
-  tags  = {
+  tags = {
     Name = "mongo-ebs-volume"
   }
 }
